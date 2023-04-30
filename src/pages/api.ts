90589ab308c2d044830fb15a14eefd77a2d17b61
george <7jagjag@gmail.com>
2023-04-30 20:04:29 +0200

    log events

--- src/pages/api.ts
+++ src/pages/api.ts
@@ -1,5 +1,9 @@
-export async function post () {
-  console.log('/api')
+import type { APIContext } from 'astro';
+
+export async function post (context: APIContext) {
+  const payload = await context.request.json()
+  console.log('/api', JSON.stringify(payload))
+
   return new Response('created', {
     status: 201,
     statusText: 'created',
