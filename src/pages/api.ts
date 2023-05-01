b91b7cbfde2df064c524e2ca7e60f8d51f5574b7
george <7jagjag@gmail.com>
2023-05-01 16:24:27 +0200

    ip and new fields

--- src/pages/api.ts
+++ src/pages/api.ts
@@ -5,11 +5,13 @@ import uap from 'ua-parser-js'
 export async function post (context: APIContext) {
   console.log('/api -> [start]')
   const payload = await context.request.json()
-  const ip = context.clientAddress
   const headers: Record<string, string> = {}
   for (const [key, value] of context.request.headers.entries()) {
     headers[key] = value
   }
+  const ip = headers['cf-connecting-ip'] || headers['x-forwarded-for'] || context.clientAddress
+  const ip_country = headers['cf-ipcountry'] || headers['x-country'] 
+  const cf_ray = headers['cf-ray']
   const ua = uap(headers['user-agent'])
   // console.log('/api', JSON.stringify(payload))
   // console.log('ua:', ua)
@@ -20,15 +22,25 @@ export async function post (context: APIContext) {
     password: process.env.DATABASE_PASSWORD,
   }
   const { d: origin, r: referer, n: event_name, u: url, p: props, s: screen } = payload
+  const [host, querystring] = url.split('?')
+  const qs: Record<string, string> = {}
+  for (const [key, value] of new URLSearchParams(querystring).entries()) {
+    qs[key] = value
+  }
   const event = {
     referer,
     origin,
     event_name,
     url,
+    url_host: host.split('/').filter(Boolean)[1],
+    url_endpoint: host.split('/').filter(Boolean)[2] || '/',
+    url_querystring: JSON.stringify(qs || {}),
     props: JSON.stringify(props || {}),
     user_agent: headers['user-agent'],
     headers: JSON.stringify(headers || {}),
     ip,
+    cf_ray,
+    ip_country,
     screen,
     browser_name: ua.browser.name,
     browser_version: ua.browser.version,
